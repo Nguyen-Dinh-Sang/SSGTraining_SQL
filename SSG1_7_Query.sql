817b8b4ec4ef9a67ced9116f63303140b427b720
DinhSang <dinhsangdroid@gmail.com>
2021-03-25 11:45:49 +0700

    SSGTraining_SQL-DinhSang: add 1.7 query

---
+++ SSG1_7_Query.sql
@@ -0,0 +1,23 @@
+﻿USE SSG1_7
+GO
+
+-- Lọc trùng
+SELECT DISTINCT JOB N'Nghề Nghiệp'
+FROM EMP
+
+-- Ghép chuỗi
+SELECT CONCAT(EMPNO, ENAME) EMPLOYEE
+FROM EMP
+
+-- Literal
+SELECT CONCAT(EMPNO, ENAME) EMPLOYEE, 'WORK IN DEPARTMENT', DEPTNO
+FROM EMP
+
+-- Gía trị Null, có một giá trị null => kết quả null, hàm nhóm sẻ bỏ qua các giá trị null
+SELECT ENAME, SAL*12 + COMM ANUAL_SAL
+FROM EMP
+
+-- 2.5 hiển thị cấu trúc bảng
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'EMP'
